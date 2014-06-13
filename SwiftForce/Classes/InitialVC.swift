2129b1694901393de858e8e10a710a2a9a535fe6
Raja Rao DV <rajaraodv100@yahoo.com>
2014-06-13 10:14:08 -0700

    first commit

---
+++ SwiftForce/Classes/InitialVC.swift
@@ -0,0 +1,40 @@
+//
+//  InitialVC.swift
+//  SwiftForce
+//
+//  Created by Raja Rao DV on 6/11/14.
+//  Copyright (c) 2014 salesforce. All rights reserved.
+//
+
+import UIKit
+
+@objc class InitialVC: UIViewController {
+
+    init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        // Custom initialization
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // #pragma mark - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SwiftForce/SwiftForce-Bridging-Header1.h
@@ -0,0 +1,8 @@
+//
+//  SwiftForce-Bridging-Header1.h
+//  SwiftForce
+//
+//  Created by Raja Rao DV on 6/13/14.
+//  Copyright (c) 2014 salesforce. All rights reserved.
+//
+#import <SalesforceNativeSDK/SFRestRequest.h>
